ea32922d628f827b2478288fd5621d6d3aae8f42
Alexander <agvolkov4@yandex.ru>
2019-05-17 23:43:19 +0300

    removed extra line :)

--- encode.js
+++ encode.js
@@ -4,7 +4,6 @@ const NodeRSA = require('node-rsa');
 fs.readFile('./source.txt', (err, buf) => {
     const text = buf.toString();
     const key = new NodeRSA({b: 512});
-    // console.log(key);
     fs.writeFile('./public.key', key.exportKey('public'), (err) => {});
     fs.writeFile('./private.key', key.exportKey('private'), (err) => {});
     fs.writeFile('./encrypted.txt', key.encrypt(text, 'base64'), (err) => {});
